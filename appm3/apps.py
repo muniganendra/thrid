8abfbef42dea40bed7ec3c7990c78538fa3bbb30
muniganendra <munigani15@gmail.com>
2022-12-24 16:47:43 +0530

    first commit

---
+++ appm3/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Appm3Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'appm3'
---
+++ appm3/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def yuva(request):
+    return HttpResponse('yuva is a good boy')
